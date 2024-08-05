98b4c68dccf0f06963bdc3227d31b8a88b14cd06
Tom Dobson <dobson980@gmail.com>
2024-08-05 13:57:38 -0700

    Initial Commit

---
+++ Catch Em All/Catch_Em_AllApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catch_Em_AllApp.swift
+//  Catch Em All
+//
+//  Created by Thomas Dobson on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Catch_Em_AllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
